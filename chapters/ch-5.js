e75dca2c8d8d8a0bdffea781d115ed657f4fd742
Driccioli <driccioli53@gmail.com>
2022-03-06 20:44:36 +0100

    Completed Ch5

--- chapters/ch-5.js
+++ chapters/ch-5.js
@@ -1,7 +1,399 @@
 chapterDialogue = [
     {
-        "character": "",
-        "dialogue" : ""
+        "character": "Gangrel",
+        "dialogue" : "What's this, then? <br>The exalt herself, in all her radiance?"
+    },
+    {
+        "character": "Gangrel",
+        "dialogue" : "I fear I must shield my eyes! Bwa ha ha ha!"
+    },
+    {
+        "character": "Emmeryn",
+        "dialogue" : "King Gangrel, I've come for the truth of this unfortunate incident between us."
+    },
+    {
+        "character": "Aversa",
+        "dialogue" : "The truth? I can give you the truth."
+    },
+    {
+        "character": "Emmeryn",
+        "dialogue" : "Perhaps milady might first share her name?"
+    },
+    {
+        "character": "Aversa",
+        "dialogue" : "You may call me Aversa."
+    },
+    {
+        "character": "Emmeryn",
+        "dialogue" : "Very well, Aversa. Is Maribelle unharmed?"
+    },
+    {
+        "character": "Gangrel",
+        "dialogue" : "Who? Oh yes, that little blonde brat."
+    },
+    {
+        "character": "Maribelle",
+        "dialogue" : "Unhand me, you gutter-born troglodyte!"
+    },
+    {
+        "character": "Lissa",
+        "dialogue" : "Maribelle!"
+    },
+    {
+        "character": "Maribelle",
+        "dialogue" : "Lissa? <br>Darling, is that you?"
+    },
+    {
+        "character": "Aversa",
+        "dialogue" : "This girl crossed the Plegian border without our consent."
+    },
+    {
+        "character": "Aversa",
+        "dialogue" : "And what's more..."
+    },
+    {
+        "character": "Aversa",
+        "dialogue" : "She wounded the brave Plegian soldiers who sought only to escort her safely home."
+    },
+    {
+        "character": "Maribelle",
+        "dialogue" : "LIES! You speak nothing but lies, hag!"
+    },
+    {
+        "character": "Maribelle",
+        "dialogue" : "Did they not teach the meaning of the word \"truth\" in wretched-crone school?!"
+    },
+    {
+        "character": "Aversa",
+        "dialogue" : "...You see? No manners at all. Such a nasty little bird simply had to be caged."
+    },
+    {
+        "character": "Gangrel",
+        "dialogue" : "Such a violent temper speaks to her guilt. <br>This will call for a weighty punishment."
+    },
+    {
+        "character": "Gangrel",
+        "dialogue" : "And if she were to later confess to being an Ylissean spy? My goodness!"
+    },
+    {
+        "character": "Gangrel",
+        "dialogue" : "It would take an act of CONSIDERABLE good faith to repair our relations."
+    },
+    {
+        "character": "Maribelle",
+        "dialogue" : "I have done nothing wrong! <br>It is they who should confess!"
+    },
+    {
+        "character": "Maribelle",
+        "dialogue" : "They are the ones who invaded Ylisse. <br>They razed an entire village!"
+    },
+    {
+        "character": "Maribelle",
+        "dialogue" : "When I attempted to intervene, they took me and dragged me across the border."
+    },
+    {
+        "character": "Maribelle",
+        "dialogue" : "Let the plundered shops and charred homes of that village serve as my proof!"
+    },
+    {
+        "character": "Gangrel",
+        "dialogue" : "That would only prove Ylisse has a bandit problem—something I hear oft of late..."
+    },
+    {
+        "character": "Gangrel",
+        "dialogue" : "But indeed, tonight I shall weep salty tears into my pillow for your dead villagers."
+    },
+    {
+        "character": "Maribelle",
+        "dialogue" : "Your Grace, please!"
+    },
+    {
+        "character": "Emmeryn",
+        "dialogue" : "Peace, Maribelle. I believe you."
+    },
+    {
+        "character": "Emmeryn",
+        "dialogue" : "King Gangrel, I request that you release this woman at once."
+    },
+    {
+        "character": "Emmeryn",
+        "dialogue" : "Surely you and I can sort out these affairs without the need of hostages."
+    },
+    {
+        "character": "Gangrel",
+        "dialogue" : "Without so much as an apology? <br>Why should I even bother with parley?"
+    },
+    {
+        "character": "Gangrel",
+        "dialogue" : "I'm within my rights to have her head this instant and be home in time for supper."
+    },
+    {
+        "character": "Chrom",
+        "dialogue" : "You black-hearted devil!"
+    },
+    {
+        "character": "Gangrel",
+        "dialogue" : "Control your dog, my dear, before he gets someone hurt."
+    },
+    {
+        "character": "Chrom",
+        "dialogue" : "Rrgh..."
+    },
+    {
+        "character": "Gangrel",
+        "dialogue" : "Now then, Your Graceliness. <br>Perhaps we can arrange a trade?"
+    },
+    {
+        "character": "Gangrel",
+        "dialogue" : "You give me the Fire Emblem, and I return Mari Contrary here in one piece."
+    },
+    {
+        "character": "Emmeryn",
+        "dialogue" : "You would ask for Ylisse's royal treasure? <br>But why?"
+    },
+    {
+        "character": "Gangrel",
+        "dialogue" : "Because I know the legend!"
+    },
+    {
+        "character": "Gangrel",
+        "dialogue" : "The Fire Emblem is the key to having all one's wishes realized."
+    },
+    {
+        "character": "Gangrel",
+        "dialogue" : "I have desired it for years. YEARS!"
+    },
+    {
+        "character": "Gangrel",
+        "dialogue" : "...Yet my birthday comes and goes each year, and nothing from Ylisse. Heheh..."
+    },
+    {
+        "character": "Emmeryn",
+        "dialogue" : "The Emblem's power is meant for a single purpose, King Gangrel:"
+    },
+    {
+        "character": "Emmeryn",
+        "dialogue" : "to save the world and its people at their hour of most desperate need."
+    },
+    {
+        "character": "Emmeryn",
+        "dialogue" : "Would you claim a more noble wish?"
+    },
+    {
+        "character": "Gangrel",
+        "dialogue" : "I want what every Plegian wants—<br>a grisly end for every last Ylissean!"
+    },
+    {
+        "character": "Gangrel",
+        "dialogue" : "What could be more noble than that?"
+    },
+    {
+        "character": "Emmeryn",
+        "dialogue" : "What?"
+    },
+    {
+        "character": "Gangrel",
+        "dialogue" : "Surely you have not forgotten what the last exalt did to my people?"
+    },
+    {
+        "character": "Gangrel",
+        "dialogue" : "Your father named us heathens!"
+    },
+    {
+        "character": "Gangrel",
+        "dialogue" : "His \"crusade\" across Plegia butchered countless of my subjects and my kin!"
+    },
+    {
+        "character": "Emmeryn",
+        "dialogue" : "...I have never denied Ylisse's past wrongdoings."
+    },
+    {
+        "character": "Emmeryn",
+        "dialogue" : "But I have sworn to never repeat those mistakes. Ours is now a realm of peace."
+    },
+    {
+        "character": "Gangrel",
+        "dialogue" : "Yours is now a haven of hypocrisy! <br>Now give me the Fire Emblem!"
+    },
+    {
+        "character": "Maribelle",
+        "dialogue" : "No, Your Grace! I'd sooner die than act as a bargaining chip for this filthy reprobate!"
+    },
+    {
+        "character": "Emmeryn",
+        "dialogue" : "No, Maribelle..."
+    },
+    {
+        "character": "Gangrel",
+        "dialogue" : "Ugh... Taaaaaalk talk-talk-talk-talk. <br>It's time to speak louder than words!"
+    },
+    {
+        "character": "Gangrel",
+        "dialogue" : "This negotiation is over, Your Luminosity!"
+    },
+    {
+        "character": "Gangrel",
+        "dialogue" : "I shall have the Emblem if I have to pry it from your shiny dead hands!"
+    },
+    {
+        "character": "Chrom",
+        "dialogue" : "Stay back! <br>Or you'll all suffer the same fate!"
+    },
+    {
+        "character": "Gangrel",
+        "dialogue" : "Now that's a declaration of war if I've ever heard one..."
+    },
+    {
+        "character": "Gangrel",
+        "dialogue" : "A big, messy war that will bleed you Ylisseans dry. Bwa ha ha!"
+    },
+    {
+        "character": "Aversa",
+        "dialogue" : "Poor, stupid girl... <br>Are you really worth fighting a war over?"
+    },
+    {
+        "character": "Aversa",
+        "dialogue" : "Years from now, you'll be remembered only as she who destroyed House Ylisse."
+    },
+    {
+        "character": "Maribelle",
+        "dialogue" : "No... That's not... <br>Oh, Lissa... Please, no..."
+    },
+    {
+        "character": "Aversa",
+        "dialogue" : "...?!"
+    },
+    {
+        "character": "Ricken",
+        "dialogue" : "Maribelle! Go! You're free!"
+    },
+    {
+        "character": "Maribelle",
+        "dialogue" : "RICKEN?! <br>What are you doing here?"
+    },
+    {
+        "character": "Ricken",
+        "dialogue" : "Just run! We can talk about it later!"
+    },
+    {
+        "character": "Aversa",
+        "dialogue" : "Oh, is this your little boyfriend? <br>Isn't he just precious."
+    },
+    {
+        "character": "Ricken",
+        "dialogue" : "Don't talk down to me, witch!"
+    },
+    {
+        "character": "Aversa",
+        "dialogue" : "Nngh! W-wind magic?"
+    },
+    {
+        "character": "Ricken",
+        "dialogue" : "Come on, Maribelle!"
+    },
+    {
+        "character": "Maribelle",
+        "dialogue" : "Right!"
+    },
+    {
+        "character": "Aversa",
+        "dialogue" : "Wretched whelp! I should..."
+    },
+    {
+        "character": "Aversa",
+        "dialogue" : "No. Our soldiers have them outnumbered."
+    },
+    {
+        "character": "Aversa",
+        "dialogue" : "The brats will be dead long before they can reach their comrades."
+    },
+    {
+        "character": "Gangrel",
+        "dialogue" : "Bwa ha ha! I have my war!"
+    },
+    {
+        "character": "Gangrel",
+        "dialogue" : "Captain Orton! Remain here, and take down as many Ylisseans as you can."
+    },
+    {
+        "character": "Gangrel",
+        "dialogue" : "You can expect reinforcements from the forts as well."
+    },
+    {
+        "character": "Gangrel",
+        "dialogue" : "Now, do your best—at doing your worst! <br>Bwa ha! "
+    },
+    {
+        "character": "Orton",
+        "dialogue" : "I won't ask for your name. Only your life!"
+    },
+    {
+        "character": "Orton",
+        "dialogue" : "This matters not. Soon war will be upon...your soil... Heh heh..."
+    },
+    {
+        "character": "Lissa",
+        "dialogue" : "Maribelle! Are you hurt?!"
+    },
+    {
+        "character": "Maribelle",
+        "dialogue" : "Nothing I didn't return twofold, darling."
+    },
+    {
+        "character": "<span class='avatar-name'>Robin</span>",
+        "dialogue" : "I'm glad you're safe."
+    },
+    {
+        "character": "Maribelle",
+        "dialogue" : "Who...? <br>Oh. It's you."
+    },
+    {
+        "character": "<span class='avatar-name'>Robin</span>",
+        "dialogue" : "Yes. I know you're not especially fond of me, but it's a relief just the same."
+    },
+    {
+        "character": "Maribelle",
+        "dialogue" : "Oh, it's not a question of fondness. <br>I am simply protective of Lissa."
+    },
+    {
+        "character": "Maribelle",
+        "dialogue" : "My treasure is very sensitive, and..."
+    },
+    {
+        "character": "Maribelle",
+        "dialogue" : "Wait. Am I really justifying myself to a commoner? Gods..."
+    },
+    {
+        "character": "Maribelle",
+        "dialogue" : "Yes, well, I do...apologize for being curt. <br>And... And..."
+    },
+    {
+        "character": "Maribelle",
+        "dialogue" : "And you have my thanks for your part in the rescue. There, I said it!"
+    },
+    {
+        "character": "Chrom",
+        "dialogue" : "Forgive me, Emm. I acted rashly."
+    },
+    {
+        "character": "Emmeryn",
+        "dialogue" : "It's all right, Chrom. King Gangrel is the one at fault here. You were only protecting me."
+    },
+    {
+        "character": "Frederick",
+        "dialogue" : "The Mad King will be rallying his forces, if they have not mobilized already."
+    },
+    {
+        "character": "Frederick",
+        "dialogue" : "I suggest we make haste back to Ylisstol and discuss our strategy."
+    },
+    {
+        "character": "Emmeryn",
+        "dialogue" : "Of course, Frederick. It seems war is upon us."
+    },
+    {
+        "character": "Emmeryn",
+        "dialogue" : "We must protect the Ylissean people at all costs."
     },
 ];
 
